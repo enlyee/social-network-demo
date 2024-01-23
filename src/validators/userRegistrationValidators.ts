507d11374c028ef02a80c2e04f61243460427360
enlyee <enlyee@icloud.com>
2024-01-23 17:50:23 +0300

    added Email Confiramtion

--- src/validators/userRegistrationValidators.ts
+++ src/validators/userRegistrationValidators.ts
@@ -1,5 +1,20 @@
 import {body} from "express-validator";
+import {usersRepository} from "../repositories/usersRepository";
 
-export const loginValidator = body('login').isLength({min: 3, max: 10}).matches('^[a-zA-Z0-9_-]*$')
+export const loginValidator = body('login').isLength({min: 3, max: 10}).matches('^[a-zA-Z0-9_-]*$').custom(async (login)=>{
+    const user = await usersRepository.findUserByLoginOrEmail(login)
+    if (user) {
+        throw new Error('This login is already exist')
+    }
+    return
+
+})
 export const passwordValidator = body('password').isLength({min: 6, max: 20})
-export const emailValidator  = body('email').matches('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')
+export const emailValidator  = body('email').matches('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$').custom(async (email)=>{
+    const user = await usersRepository.findUserByLoginOrEmail(email)
+    if (user) {
+        throw new Error('This email is already exist')
+    }
+    return
+
+})
